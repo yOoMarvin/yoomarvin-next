b7ff2965aaaf6b3b2fba7bd977db96a70c0d54d9
yoomarvin <marvin.messenzehl@gmail.com>
2019-10-10 19:54:32 +0200

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,16 @@
 module.exports = {
-  extends: ["plugin:prettier/recommended", "airbnb-base"]
+  extends: ["plugin:prettier/recommended", "airbnb-base"],
+  rules: {
+    "import/prefer-default-export": 0,
+    "space-before-function-paren": 0,
+    "comma-dangle": 0,
+    quotes: [
+      2,
+      "double",
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true
+      }
+    ]
+  }
 };
