1490f47327b574a51c2122ef67943de07d9c8fb0
yoomarvin <marvin.messenzehl@gmail.com>
2019-11-12 21:58:16 +0100

    and some more posts!

---
+++ data/blog/KeepingTheStructure/content.js
@@ -0,0 +1,34 @@
+const text = `
+[Last times](http://marvinmessenzehl.com/work/2017/09/03/EverydayTools.html) post was all about tooling and the products and little helpers I'm using in my everyday life. But actually... I have to be honest... **tools don't matter!** It's more about the creating aspect. Tools will change. Always! But the methods, your mindset, and your abilities will stay. Don't focus on your toolset, instead focus on the stuff you want to create!     
+Today we're going to dive in one important aspect of this all. The aspect that maybe holds everything together. I call it **keeping the structure**. So, let's grab the reading glasses, find a cozy place and let's explore!
+
+### Wait. Do we need tools again?
+Ok, things get a little weird here. First I'm saying _"oh, tools are great!"_ Than I'm like _" but actually you should not focus on your tools"_ But for this "organizing myself" thing I really like to use some system, supported by special tools. I like to call it some kind of **outsourcing**. Because it makes everything so much easier. By outsourcing, I mean grabbing all this organization and to do stuff and put it out of your brain. So you can make space for more important things, be more creative and come up with new ideas. Like shifting the focus, you know?
+
+First, this approach felt strange and I thought that this would be unproductive because I must look in my calendar or notebook for every single task and meeting. But I was so wrong. It felt so great. The focus is clear and the separation and outsourcing of this boring stuff are so effective. I really saw the benefits after some time!
+
+### The problem set. Too many sources
+So we're now into this article some time and it still isn't 100% clear about what it actually is. By **keeping the structure** I mean everything that is related to organizing yourself. Your to-dos, your notes, your meetings, and events... everything. Because in our time so much is around I found myself often freaking out because there was so much! But the worst was... **too many sources!** This was so annoying. Some todos here, then some planning in my calendar, some notes in that app, some notes on that paper thing. It was terrible. And I'm actually a person that really puts some weight on keeping organized.  
+
+But some time ago I found the perfect solution for me. To be clear. This is the system and workflow **I'm using** for myself. That does not mean that it will work for you and everyone else. But if you can find yourself a bit in this article, feel free to try it out!   
+
+### The 3 pillars
+My system contains now 3 pillars. The foundation of everything is my neat little notebook with the [Bullet Journal System](http://bulletjournal.com/). Here I'm using a _DIN A5 Leuchtturm notebook with dotted paper_. A lot of people are really artful when it comes to note-taking and styling their bullet journal, but I've just wanted to keep things effective.  
+
+---
+
+The first part of the bullet journal consists of the **future log** Here you plan ahead 6 months and write down some very important tasks and events for the respective month. The next part is the so-called **monthly log**. Here you see all tasks and events for the actual month. The next step is the **weekly log**. This is my smallest unit. Here I'm planning my week. Every Sunday I sit down, making a new weekly log and plan ahead for the week. So I see what I have to do. This is also a nice way to outsource all these thoughts and stuff from your brain into your notebook. At the end of the month, it comes to the bullet journal **migrating** process. But you can read a lot more about this awesome system here. I just picked some stuff out for myself. Oh and I'm also writing all my notes [here](http://bulletjournal.com/). Everything from blogpost ideas, app sketches, project ideas. For serious note taking at work, I'm using some kind of [Cornell Method](https://en.wikipedia.org/wiki/Cornell_Notes), where I write a short summary of key points of every meeting. At the end of the day, my bullet journal is the go-to tool when it comes to organizing and planning.
+
+The second pillar is the digital twin of my bullet journal. Because I don't have my notebook all the time in reach and I would like to reach my tasks from several platforms I'm using a classical todo list here. For me, [todoist](https://todoist.com/) works perfectly! I talked about it in my [last post](http://marvinmessenzehl.com/work/2017/09/03/EverydayTools.html), so I won't cover it in depth here. Every day I'm checking my bullet journal for open tasks and transfer them to my todo list. This is also a nice task for reminding yourself of the things you have to do!   
+
+The third pillar is the archive. At the end of the month, all my notes will be transferred to [Evernote](https://evernote.com). This is my container for everything and everything else and it works as my single source of truth. Everything lands here, so I know where to look things up!
+
+---
+
+### Still improving
+This is the system and workflow I'm using now since the beginning of the year. And it works really well! Sure I made some adjustments over the time and still try to improve things, but for now, it really goes well! I'm feeling a lot more organized now and can also plan nicely ahead.
+If you have any questions, feel free to contact me on Twitter!
+Thank you for reading! And **keep creating awesome things!**
+`;
+
+export default text;
--- data/blog/index.js
+++ data/blog/index.js
@@ -3,11 +3,17 @@ import RedesigningMySite from "./RedesigningMySite";
 import MyJourneyWithCode from "./MyJourneyWithCode";
 import MyJourneyWithDesign from "./MyJourneyWithDesign";
 import EverydayTools from "./EverydayTools";
+import KeepingTheStructure from "./KeepingTheStructure";
+import Innohack2017 from "./Innohack2017";
+import InTheLandOfTheory from "./InTheLandOfTheory";
 
 export default [
   DaysOfCode,
   RedesigningMySite,
   MyJourneyWithCode,
   MyJourneyWithDesign,
-  EverydayTools
+  EverydayTools,
+  KeepingTheStructure,
+  Innohack2017,
+  InTheLandOfTheory
 ].reverse();
