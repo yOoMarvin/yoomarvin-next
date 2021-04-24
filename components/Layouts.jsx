2957180021cbb59e6f01cd45251117a2dd46020f
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2021-04-24 08:14:08 +0200

    adding prettier

--- components/Layouts.jsx
+++ components/Layouts.jsx
@@ -1,9 +1,9 @@
 import * as React from 'react'
 
-export function CenteredColumn ({ children }) {
-  return (
-    <div className='flex flex-col max-w-screen-sm mx-auto justify-content'>
-      {children}
-    </div>
-  )
+export function CenteredColumn({ children }) {
+    return (
+        <div className="flex flex-col max-w-screen-sm mx-auto justify-content">
+            {children}
+        </div>
+    )
 }
