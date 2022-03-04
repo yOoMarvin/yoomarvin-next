42b70fc551cf3182f53261b1277a38c8e5938b31
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2022-03-04 16:53:28 +0100

    upgrade prettier & tailwind + cleanup old files

--- components/Layouts.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-
-export function CenteredColumn({ children }) {
-    return (
-        <div className="flex flex-col max-w-screen-sm mx-auto justify-content">
-            {children}
-        </div>
-    )
-}
