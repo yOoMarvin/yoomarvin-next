c14bc1405ed67532ab8574dc83b0cc276cdf845a
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2022-04-21 14:48:59 +0200

    include reportic analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import Script from 'next/script'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Script
+                type="text/javascript"
+                src="https://cdn.reportic.de/r.js"
+                data-site="338191"
+            />
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp
