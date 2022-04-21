9275eab57b7bb2622c1cfaa4275381273b8924c5
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2022-04-21 14:54:55 +0200

    change to nextjs head tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
+import Head from 'next/head'
 import Script from 'next/script'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
     return (
         <>
-            <Script
-                type="text/javascript"
-                src="https://cdn.reportic.de/r.js"
-                data-site="338191"
-            />
+            <Head>
+                <Script
+                    type="text/javascript"
+                    src="https://cdn.reportic.de/r.js"
+                    data-site="411867"
+                />
+            </Head>
             <Component {...pageProps} />
         </>
     )
