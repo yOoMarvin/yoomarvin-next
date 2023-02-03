1918e0ecb6e5900ddb72c778a1d0a4eb458c2c84
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2023-02-03 20:02:39 +0100

    Restructuring header and navigation

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import NavLink from '../components/NavLink'
 import ToggleModeButton from '../components/ToggleModeButton'
+import Image from 'next/image'
+import Link from 'next/Link'
 
 export default function RootLayout({ children }) {
     //Layouts must accept a children prop.
@@ -9,9 +11,11 @@ export default function RootLayout({ children }) {
         <html lang="en">
             <body className="mx-auto max-w-screen-lg p-2">
                 <header>
-                    <div className="mb-8 flex flex-row items-center justify-between">
-                        <div />
-                        <div className="flex flex-row items-center space-x-2 rounded-md bg-neutral-50 p-0.5 dark:bg-opacity-5">
+                    <div className="top-0 mb-8 flex flex-row items-center justify-between py-2 backdrop-blur-lg">
+                        <Link href="/">
+                            <div className="h-10 w-10 rounded-full bg-neutral-500" />
+                        </Link>
+                        <div className="flex flex-row items-center space-x-2">
                             <NavLink href="/about">About</NavLink>
                             <NavLink href="/blog">Blog</NavLink>
                             <NavLink href="/work">Work</NavLink>
