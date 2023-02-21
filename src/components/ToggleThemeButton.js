adf7ef555be09d5481a7d77d346bd58caa93401f
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2023-02-21 22:42:09 +0100

    Adding a toggle animation to switch the Theme

--- src/components/ToggleThemeButton.js
+++ src/components/ToggleThemeButton.js
@@ -3,42 +3,50 @@
 import { MoonIcon, SunIcon } from '@heroicons/react/24/solid'
 import { useTheme } from 'next-themes'
 import { useState, useEffect } from 'react'
+import { motion } from 'framer-motion'
 
 export default function ToggleThemeButton() {
     const { systemTheme, theme, setTheme } = useTheme()
+    const [isOn, setIsOn] = useState(false)
 
-    //avoid hydration mismatch
-    const [mounted, setMounted] = useState(false)
+    const toggleSwitch = () => {
+        setIsOn(!isOn)
+        const currentTheme = theme === 'system' ? systemTheme : theme
+        currentTheme === 'dark' ? setTheme('light') : setTheme('dark')
+    }
+
+    const spring = {
+        type: 'spring',
+        stiffness: 700,
+        damping: 30,
+    }
 
     useEffect(() => {
         setMounted(true)
     }, [])
 
-    function themeChanger() {
-        if (!mounted) return null
-
-        const currentTheme = theme === 'system' ? systemTheme : theme
-
-        if (currentTheme === 'dark') {
-            return (
-                <SunIcon
-                    className="transition-base h-6 text-neutrals-500 hover:text-text-primary"
-                    onClick={() => setTheme('light')}
-                />
-            )
-        } else {
-            return (
-                <MoonIcon
-                    className="transition-base h-6 text-neutrals-500 hover:text-text-primary "
-                    onClick={() => setTheme('dark')}
-                />
-            )
-        }
-    }
-
     return (
-        <button type="button" className="p-2">
-            {themeChanger()}
-        </button>
+        <div>
+            {/* <button type="button" className="p-2">
+                {themeChanger()}
+            </button> */}
+            <div
+                className="flex h-10 w-16 cursor-pointer flex-row justify-start rounded-full border border-neutrals-50 p-1"
+                data-isOn={isOn}
+                onClick={toggleSwitch}
+            >
+                <motion.div
+                    className="h-8 w-8 rounded-full bg-neutrals-default"
+                    layout
+                    transition={spring}
+                >
+                    {theme === 'dark' ? (
+                        <MoonIcon className="ml-1.5 mt-1.5 h-5 text-text-primary" />
+                    ) : (
+                        <SunIcon className="ml-1.5 mt-1.5 h-5 text-text-primary" />
+                    )}
+                </motion.div>
+            </div>
+        </div>
     )
 }
