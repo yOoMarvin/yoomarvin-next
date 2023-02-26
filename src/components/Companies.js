09717322c28e53f424ee1946c68899927580a6f6
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2023-02-26 13:00:42 +0100

    Refactoring the companie logo section and removing some old logos

--- src/components/Companies.js
+++ src/components/Companies.js
@@ -7,10 +7,10 @@ export default function Companies() {
     const { resolvedTheme } = useTheme()
 
     return (
-        <div className="w-full rounded-2xl bg-neutrals-50 p-8">
+        <div className="opacity-40">
             <div className="alignt-start grid grid-cols-3 items-center justify-center gap-4 sm:grid-cols-3">
                 <Image
-                    className="max-h-6 w-full object-contain opacity-60 brightness-0 grayscale filter"
+                    className="max-h-6 w-full object-contain brightness-0 grayscale filter"
                     style={{
                         filter:
                             resolvedTheme == 'dark'
@@ -24,34 +24,7 @@ export default function Companies() {
                 />
 
                 <Image
-                    className="max-h-6 w-full object-contain opacity-60 brightness-0 grayscale filter"
-                    style={{
-                        filter:
-                            resolvedTheme == 'dark'
-                                ? 'invert(100%)'
-                                : 'invert(0)',
-                    }}
-                    src="/companies/hello.png"
-                    alt="Hello Design"
-                    height={20}
-                    width={80}
-                />
-
-                <Image
-                    className="max-h-8 w-full object-contain opacity-50 brightness-0 grayscale filter"
-                    style={{
-                        filter:
-                            resolvedTheme == 'dark'
-                                ? 'invert(100%)'
-                                : 'invert(0)',
-                    }}
-                    src="/companies/lbs.png"
-                    alt="Landes Bausparkasse"
-                    height={24}
-                    width={80}
-                />
-                <Image
-                    className="max-h-6 w-full object-contain opacity-60 brightness-0 grayscale filter"
+                    className="max-h-6 w-full object-contain brightness-0 grayscale filter"
                     style={{
                         filter:
                             resolvedTheme == 'dark'
@@ -64,7 +37,7 @@ export default function Companies() {
                     width={80}
                 />
                 <Image
-                    className="max-h-4 w-full object-contain opacity-60 brightness-0 grayscale filter"
+                    className="max-h-4 w-full object-contain brightness-0 grayscale filter"
                     style={{
                         filter:
                             resolvedTheme == 'dark'
@@ -76,21 +49,9 @@ export default function Companies() {
                     height={16}
                     width={80}
                 />
+
                 <Image
-                    className="max-h-4 w-full object-contain opacity-60 brightness-0 grayscale filter"
-                    style={{
-                        filter:
-                            resolvedTheme == 'dark'
-                                ? 'invert(100%)'
-                                : 'invert(0)',
-                    }}
-                    src="/companies/onvy.png"
-                    alt="ONVY Health"
-                    height={16}
-                    width={80}
-                />
-                <Image
-                    className="max-h-4 w-full object-contain opacity-60 contrast-200 grayscale filter"
+                    className="max-h-4 w-full object-contain contrast-200 grayscale filter"
                     style={{
                         filter:
                             resolvedTheme == 'dark'
@@ -102,21 +63,9 @@ export default function Companies() {
                     height={16}
                     width={80}
                 />
+
                 <Image
-                    className="max-h-8 w-full object-contain opacity-60 brightness-0 grayscale filter"
-                    style={{
-                        filter:
-                            resolvedTheme == 'dark'
-                                ? 'grayscale(100%) invert(100%)'
-                                : 'grayscale(100%) invert(0)',
-                    }}
-                    src="/companies/stuttgart.png"
-                    alt="Börse Stuttgart"
-                    height={24}
-                    width={80}
-                />
-                <Image
-                    className="max-h-8 w-full object-contain opacity-60 brightness-0 grayscale filter"
+                    className="max-h-6 w-full object-contain brightness-0 grayscale filter"
                     style={{
                         filter:
                             resolvedTheme == 'dark'
@@ -125,7 +74,20 @@ export default function Companies() {
                     }}
                     src="/companies/sas.png"
                     alt="SAS Analytics"
-                    height={24}
+                    height={20}
+                    width={80}
+                />
+                <Image
+                    className="max-h-6 w-full object-contain brightness-0 grayscale filter"
+                    style={{
+                        filter:
+                            resolvedTheme == 'dark'
+                                ? 'grayscale(100%) invert(100%)'
+                                : 'grayscale(100%) invert(0)',
+                    }}
+                    src="/companies/tedx.png"
+                    alt="TEDx"
+                    height={20}
                     width={80}
                 />
             </div>
