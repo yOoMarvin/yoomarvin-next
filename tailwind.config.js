f906797e4b88de6d7d126aa06c9271cff0b6a00c
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2024-04-15 15:24:38 +0000

    remove all styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,26 +8,6 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                neutrals: {
-                    50: 'var(--color-neutrals-50)',
-                    100: 'var(--color-neutrals-100)',
-                    200: 'var(--color-neutrals-200)',
-                    300: 'var(--color-neutrals-300)',
-                    400: 'var(--color-neutrals-400)',
-                    500: 'var(--color-neutrals-500)',
-                    default: 'var(--color-neutrals-default)',
-                },
-                text: {
-                    primary: 'var(--color-text-primary)',
-                    secondary: 'var(--color-text-secondary)',
-                    tertiary: 'var(--color-text-tertiary)',
-                    action: 'var(--color-text-action)',
-                },
-                action: {
-                    primary: 'var(--color-action-primary)',
-                    secondary: 'var(--color-action-primary)',
-                },
-                //new color styles below
                 background: {
                     default: 'var(--color-background-default)',
                     variant: 'var(--color-background-variant)',
