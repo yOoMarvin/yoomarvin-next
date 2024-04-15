0286f84ab949ad7d281315c0185bb02aeb3fd8d5
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2024-04-15 14:18:26 +0000

    adding new styles to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,39 @@ module.exports = {
                     primary: 'var(--color-action-primary)',
                     secondary: 'var(--color-action-primary)',
                 },
+                //new color styles below
+                background: {
+                    default: 'var(--color-background-default)',
+                    variant: 'var(--color-background-variant)',
+                    highlight: 'var(--color-background-highlight)',
+                },
+                onBackground: {
+                    high: 'var(--color-on-background-high)',
+                    medium: 'var(--color-on-background-medium)',
+                    low: 'var(--color-on-background-low)',
+                },
+                ui: {
+                    low: 'var(--color-ui-low)',
+                    neutral: 'var(--color-ui-neutral)',
+                    medium: 'var(--color-ui-medium)',
+                    high: 'var(--color-ui-high)',
+                    highlight: 'var(--color-ui-highlight)',
+                    highlightHover: 'var(--color-ui-dhighlight-hover)',
+                },
+                onUi: {
+                    low: 'var(--color-on-ui-low)',
+                    medium: 'var(--color-on-ui-medium)',
+                    high: 'var(--color-on-ui-high)',
+                },
+                onUiHighlight: {
+                    low: 'var(--color-on-ui-highlight-low)',
+                    medium: 'var(--color-on-uihighlight--medium)',
+                    high: 'var(--color-on-ui-highlight-high)',
+                },
+                special: {
+                    link: 'var(--color-special-link)',
+                    linkHover: 'var(--color-special-link-hover)',
+                },
             },
         },
     },
