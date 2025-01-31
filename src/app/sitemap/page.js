df5e2201d3113ba06d7bba7fc873ceeae8a70436
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2025-01-31 15:36:13 +0100

    Adding the fighill manual

--- src/app/sitemap/page.js
+++ src/app/sitemap/page.js
@@ -1,11 +1,24 @@
+import Link from 'next/link'
+
 export default function SitemapPage() {
     return (
-        <section>
-            <h1 className="section__title">Sitemap</h1>
-            <p>
-                I'll chuck some links up here soon, so you can easily find those
-                not-so-obvious pages.
-            </p>
-        </section>
+        <>
+            <section>
+                <h1 className="section__title">Sitemap</h1>
+                <p>
+                    I'll chuck some links up here soon, so you can easily find
+                    those not-so-obvious pages, too.
+                </p>
+            </section>
+
+            <section>
+                <h2 className="section__subtitle">Sideprojects</h2>
+                <Link href="/fighill" className="link-basic">
+                    FigHill
+                </Link>{' '}
+                - My very first Figma widget, that makes design progress
+                visible.
+            </section>
+        </>
     )
 }
