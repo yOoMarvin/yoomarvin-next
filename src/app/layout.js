10155bb167ae4f2469caa83bb72c86120571ffbb
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2025-05-15 13:52:41 +0200

    Adding analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import Footer from 'src/components/Footer'
 import Navigation from 'src/components/Navigation'
 import Provider from 'src/components/Provider'
 import '../styles/globals.css'
+import { Analytics } from '@vercel/analytics/next'
 
 export default function RootLayout({ children }) {
     //Layouts must accept a children prop.
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
                     <Navigation />
                     <main className="mx-auto mt-24 max-w-screen-md px-2 sm:px-4 md:px-2">
                         {children}
+                        <Analytics />
                     </main>
                     <Footer />
                 </Provider>
